ee2d4851fa0d1941125f98d623dee3b23a7f8556
Ruben Verborgh <ruben@verborgh.org>
2014-07-29 23:16:24 +0200

    Add basic search functionality.

--- hdt.cc
+++ hdt.cc
@@ -12,6 +12,16 @@ Handle<Value> VersionGetter(Local<String> property, const AccessorInfo& info) {
   return scope.Close(String::New("HDT native"));
 }
 
+// Parses an argument as an RDF entity (IRI, literal, or blank/variable/empty)
+const string ParseEntityArgument(const Local<Value>& value) {
+  if (value->IsString()) {
+    const String::Utf8Value utf8Value(value);
+    const string entity(*utf8Value);
+    return entity;
+  }
+  return "";
+}
+
 // Searches for a triple pattern in an HDT file.
 // JavaScript signature: search(filename, subject, predicate, object)
 Handle<Value> Search(const Arguments& args) {
@@ -23,6 +33,9 @@ Handle<Value> Search(const Arguments& args) {
     return scope.Close(Undefined());
   }
   const String::Utf8Value filename(args[0]);
+  const string subject   = args.Length() > 1 ? ParseEntityArgument(args[1]) : "";
+  const string predicate = args.Length() > 2 ? ParseEntityArgument(args[2]) : "";
+  const string object    = args.Length() > 3 ? ParseEntityArgument(args[3]) : "";
 
   // Open the HDT file
   HDT *hdt;
@@ -31,9 +44,22 @@ Handle<Value> Search(const Arguments& args) {
     ThrowException(Exception::TypeError(String::New(error)));
     return scope.Close(Undefined());
   }
+
+  // Fill array with result triples
+  Handle<Array> triples = Array::New(0);
+  IteratorTripleString *it = hdt->search(subject.c_str(), predicate.c_str(), object.c_str());
+  for (long count = 0; it->hasNext(); count++) {
+    TripleString *triple = it->next();
+    Handle<Object> object = Object::New();
+    object->Set(String::NewSymbol("subject"),   String::New(triple->getSubject().c_str()));
+    object->Set(String::NewSymbol("predicate"), String::New(triple->getPredicate().c_str()));
+    object->Set(String::NewSymbol("object"),    String::New(triple->getObject().c_str()));
+    triples->Set(count, object);
+  }
+  delete it;
   delete hdt;
 
-  return scope.Close(Undefined());
+  return scope.Close(triples);
 }
 
 // Exposes members on the main module
--- test/hdt-test.js
+++ test/hdt-test.js
@@ -41,5 +41,71 @@ describe('hdt', function () {
         .should.throw('Non-HDT Section');
       });
     });
+
+    describe('when called with an HDT file as argument', function () {
+      describe('with pattern none none none', function () {
+        it('should return an array with matches', function () {
+          var triples = hdt.search('./test/test.hdt');
+          triples.should.be.an.Array;
+          triples.should.have.lengthOf(10);
+          triples[0].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri4' });
+        });
+      });
+
+      describe('with pattern ex:uri3 none none', function () {
+        it('should return an array with matches', function () {
+          var triples = hdt.search('./test/test.hdt', 'http://example.org/uri3');
+          triples.should.be.an.Array;
+          triples.should.have.lengthOf(2);
+          triples[0].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri4' });
+          triples[1].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri5' });
+        });
+      });
+
+      describe('with pattern ex:uri3 null null', function () {
+        it('should return an array with matches', function () {
+          var triples = hdt.search('./test/test.hdt', 'http://example.org/uri3', null, null);
+          triples.should.be.an.Array;
+          triples.should.have.lengthOf(2);
+          triples[0].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri4' });
+          triples[1].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri5' });
+        });
+      });
+
+      describe('with pattern null ex:predicate3 null', function () {
+        it('should return an array with matches', function () {
+          var triples = hdt.search('./test/test.hdt', null, 'http://example.org/predicate3', null);
+          triples.should.be.an.Array;
+          triples.should.have.lengthOf(2);
+          triples[0].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri4' });
+          triples[1].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri5' });
+        });
+      });
+
+      describe('with pattern null null ex:uri4', function () {
+        it('should return an array with matches', function () {
+          var triples = hdt.search('./test/test.hdt', null, null, 'http://example.org/uri4');
+          triples.should.be.an.Array;
+          triples.should.have.lengthOf(1);
+          triples[0].should.eql({ subject:   'http://example.org/uri3',
+                                  predicate: 'http://example.org/predicate3',
+                                  object:    'http://example.org/uri4' });
+        });
+      });
+    });
   });
 });
