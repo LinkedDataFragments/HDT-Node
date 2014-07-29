8820eda3dbe7219edfac98047e536445c6970e93
Ruben Verborgh <ruben@verborgh.org>
2014-07-29 21:37:57 +0200

    Start search function with arguments checking.

--- hdt.cc
+++ hdt.cc
@@ -1,6 +1,9 @@
 #include <node.h>
 #include <v8.h>
+#include <iostream>
+#include <HDTManager.hpp>
 
+using namespace hdt;
 using namespace v8;
 
 // Gets the version of the module.
@@ -9,8 +12,33 @@ Handle<Value> VersionGetter(Local<String> property, const AccessorInfo& info) {
   return scope.Close(String::New("HDT native"));
 }
 
+// Searches for a triple pattern in an HDT file.
+// JavaScript signature: search(filename, subject, predicate, object)
+Handle<Value> Search(const Arguments& args) {
+  HandleScope scope;
+
+  // Parse arguments
+  if (args.Length() == 0 || !args[0]->IsString()) {
+    ThrowException(Exception::TypeError(String::New("search should receive a string as first argument.")));
+    return scope.Close(Undefined());
+  }
+  const String::Utf8Value filename(args[0]);
+
+  // Open the HDT file
+  HDT *hdt;
+  try { hdt = HDTManager::mapHDT(*filename); }
+  catch (const char* error) {
+    ThrowException(Exception::TypeError(String::New(error)));
+    return scope.Close(Undefined());
+  }
+  delete hdt;
+
+  return scope.Close(Undefined());
+}
+
 // Exposes members on the main module
 void InitializeHdtModule(Handle<Object> exports) {
   exports->SetAccessor(String::NewSymbol("version"), VersionGetter, NULL);
+  exports->Set(String::NewSymbol("search"), FunctionTemplate::New(Search)->GetFunction());
 }
 NODE_MODULE(hdt, InitializeHdtModule)
--- test/hdt-test.js
+++ test/hdt-test.js
@@ -12,4 +12,34 @@ describe('hdt', function () {
       hdt.version.should.equal('HDT native');
     });
   });
+
+  describe('search', function () {
+    describe('when called without arguments', function () {
+      it('should throw an error', function () {
+        (function () { hdt.search() })
+        .should.throw('search should receive a string as first argument.');
+      });
+    });
+
+    describe('when called with a non-string argument', function () {
+      it('should throw an error', function () {
+        (function () { hdt.search(null) })
+        .should.throw('search should receive a string as first argument.');
+      });
+    });
+
+    describe('when called with a non-existing file as argument', function () {
+      it('should throw an error', function () {
+        (function () { hdt.search('abc') })
+        .should.throw('Error opening HDT file for mapping.');
+      });
+    });
+
+    describe('when called with a non-HDT file as argument', function () {
+      it('should throw an error', function () {
+        (function () { hdt.search('./test/hdt-test.js') })
+        .should.throw('Non-HDT Section');
+      });
+    });
+  });
 });
