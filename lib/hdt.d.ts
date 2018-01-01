3341474d45c620eb779c0f8cca2126bce6aa80af
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-01-01 18:02:18 +0100

    added typescript def

---
+++ lib/hdt.d.ts
@@ -0,0 +1,44 @@
+declare module "hdt" {
+  export interface Statement {
+    subject: string;
+    predicate: string;
+    object: string;
+  }
+
+  export interface SearchTermsOpts {
+    limit?: number;
+    position?: "subject" | "predicate" | "object";
+    prefix?: string;
+  }
+
+  export interface SearchLiteralsOpts {
+    limit?: number;
+    offset?: number;
+  }
+
+  export interface SearchLiteralsResult {
+    literals: string[];
+    totalCount: number;
+  }
+
+  export interface SearchTriplesOpts {
+    limit?: number;
+    offset?: number;
+  }
+
+  export interface SearchResult {
+    triples: Statement[];
+    totalCount: number;
+    hasExactCount: boolean;
+  }
+
+  export interface Document {
+    searchTriples(sub?: string, pred?: string, obj?: string, opts?: SearchTriplesOpts): Promise<SearchResult>;
+    countTriples(sub?: string, pred?: string, obj?: string): Promise<SearchResult>;
+    searchLiterals(substring: string, opts?: SearchLiteralsOpts): Promise<SearchLiteralsResult>;
+    searchTerms(opts?: SearchTermsOpts): Promise<string[]>;
+    close(): Promise<void>;
+  }
+  
+  export function fromFile(filename: string): Promise<Document>;
+}
