476c2202dd83ea6c2d12ed7348006a9f68df8553
dinikolop <dimnik90@gmail.com>
2018-11-05 08:19:40 +0100

    Fix wrong declaration

--- lib/HdtDocument.cc
+++ lib/HdtDocument.cc
@@ -518,8 +518,8 @@ public:
         const char* predicate = reinterpret_cast<char*>(terms->next());
 
         // Check whether a triple with this predicate and subject or object exists
-        IteratorUCharString *it = subject.empty() ? document->GetHDT()->search("", predicate, object.c_str())
-                                                  : document->GetHDT()->search(subject.c_str(), predicate, "");
+        hdt::IteratorTripleString *it = subject.empty() ? document->GetHDT()->search("", predicate, object.c_str())
+                                                        : document->GetHDT()->search(subject.c_str(), predicate, "");
         if (it->hasNext())
           distinctTerms.push_back(predicate);
         delete it;
