b03dd3807bb6b763028658f8a62d34fd37dc89f7
Ruben Taelman <ruben.taelman@ugent.be>
2024-03-25 12:14:52 +0100

    Migrate typings to @rdfjs/types
    This also removes the namespace, which is considered bad practise now in
    TS.

--- lib/hdt.d.ts
+++ lib/hdt.d.ts
@@ -1,44 +1,48 @@
-import * as RDF from "rdf-js";
+import * as RDF from "@rdfjs/types";
 
-declare module "hdt" {
-  export interface SearchTermsOpts {
-    limit?: number;
-    position?: "subject" | "predicate" | "object";
-    prefix?: string;
-    subject?: string; // mutually exclusive with prefix and prioritized
-    object?: string, // mutually exclusive with prefix and prioritized
-  }
-
-  export interface SearchLiteralsOpts {
-    limit?: number;
-    offset?: number;
-  }
-
-  export interface SearchLiteralsResult {
-    literals: RDF.Literal[];
-    totalCount: number;
-  }
-
-  export interface SearchTriplesOpts {
-    limit?: number;
-    offset?: number;
-  }
-
-  export interface SearchResult {
-    triples: RDF.Quad[];
-    totalCount: number;
-    hasExactCount: boolean;
-  }
-
-  export interface Document {
-    searchTriples(sub?: RDF.Term, pred?: RDF.Term, obj?: RDF.Term, opts?: SearchTriplesOpts): Promise<SearchResult>;
-    countTriples(sub?: RDF.Term, pred?: RDF.Term, obj?: RDF.Term): Promise<SearchResult>;
-    searchLiterals(substring: string, opts?: SearchLiteralsOpts): Promise<SearchLiteralsResult>;
-    searchTerms(opts?: SearchTermsOpts): Promise<string[]>;
-    close(): Promise<void>;
-    readHeader(): Promise<string>;
-    changeHeader(triples:string, outputFile:string): Promise<Document>;
-  }
-
-  export function fromFile(filename: string, opts?: { dataFactory?: RDF.DataFactory }): Promise<Document>;
+export interface SearchTermsOpts {
+  limit?: number;
+  position?: "subject" | "predicate" | "object";
+  prefix?: string;
+  subject?: string; // mutually exclusive with prefix and prioritized
+  object?: string, // mutually exclusive with prefix and prioritized
 }
+
+export interface SearchLiteralsOpts {
+  limit?: number;
+  offset?: number;
+}
+
+export interface SearchLiteralsResult {
+  literals: RDF.Literal[];
+  totalCount: number;
+}
+
+export interface SearchTriplesOpts {
+  limit?: number;
+  offset?: number;
+}
+
+export interface SearchResult {
+  triples: RDF.Quad[];
+  totalCount: number;
+  hasExactCount: boolean;
+}
+
+export interface Document {
+  searchTriples(sub?: RDF.Term, pred?: RDF.Term, obj?: RDF.Term, opts?: SearchTriplesOpts): Promise<SearchResult>;
+
+  countTriples(sub?: RDF.Term, pred?: RDF.Term, obj?: RDF.Term): Promise<SearchResult>;
+
+  searchLiterals(substring: string, opts?: SearchLiteralsOpts): Promise<SearchLiteralsResult>;
+
+  searchTerms(opts?: SearchTermsOpts): Promise<string[]>;
+
+  close(): Promise<void>;
+
+  readHeader(): Promise<string>;
+
+  changeHeader(triples: string, outputFile: string): Promise<Document>;
+}
+
+export function fromFile(filename: string, opts?: { dataFactory?: RDF.DataFactory }): Promise<Document>;
